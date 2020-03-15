70aa98dc90a883f9c9bc00dd89c33150b05ebf78
Paul Griffiths <paul@paulgriffiths.net>
2020-03-15 13:01:14 -0400

    Add first version of public template.

--- public_template.go
+++ public_template.go
@@ -1,7 +1,233 @@
 package pgtpm
 
+import (
+	"encoding/binary"
+	"math/big"
+
+	"github.com/google/go-tpm/tpm2"
+	"github.com/google/go-tpm/tpmutil"
+)
+
+// PublicTemplate marshals/unmarshals to/from the JSON-encoding of a
+// tpm2.Public object.
 type PublicTemplate struct {
-	Type       Algorithm         `json:"type"`
-	NameAlg    Algorithm         `json:"name_alg"`
-	Attributes []ObjectAttribute `json:"attributes,omitempty"`
+	Type                Algorithm         `json:"type"`
+	NameAlg             Algorithm         `json:"name_alg"`
+	Attributes          []ObjectAttribute `json:"attributes,omitempty"`
+	RSAParameters       *RSAParams        `json:"rsa,omitempty"`
+	ECCParameters       *ECCParams        `json:"ecc,omitempty"`
+	SymCipherParameters *SymCipherParams  `json:"sym_cipher,omitempty"`
+	KeyedHashParameters *KeyedHashParams  `json:"keyed_hash,omitempty"`
+}
+
+// RSAParams represents parameters of an RSA key pair.
+type RSAParams struct {
+	Symmetric *SymScheme `json:"symmetric,omitempty"`
+	Sign      *SigScheme `json:"scheme,omitempty"`
+	KeyBits   uint16     `json:"key_bits"`
+	Exponent  uint32     `json:"exponent"`
+	Modulus   *big.Int   `json:"modulus,omitempty"`
+}
+
+// ECCParams represents parameters of an ECC key pair.
+type ECCParams struct {
+	Symmetric *SymScheme    `json:"symmetric,omitempty"`
+	Sign      *SigScheme    `json:"scheme,omitempty"`
+	CurveID   EllipticCurve `json:"elliptic_curve"`
+	KDF       *KDFScheme    `json:"kdf,omitempty"`
+	Point     *ECPoint      `json:"point,omitempty"`
+}
+
+// SymCipherParams represents parameters of a symmetric cipher TPM object.
+type SymCipherParams struct {
+	Symmetric *SymScheme `json:"symmetric,omitempty"`
+}
+
+// KeyedHashParams represents parameters of a keyed hash TPM object.
+type KeyedHashParams struct {
+	Alg  Algorithm `json:"algorithm"`
+	Hash Algorithm `json:"hash"`
+	KDF  Algorithm `json:"kdf"`
+}
+
+// SymScheme represents a symmetric encryption scheme.
+type SymScheme struct {
+	Alg     Algorithm `json:"algorithm"`
+	KeyBits uint16    `json:"key_bits"`
+	Mode    Algorithm `json:"mode"`
+}
+
+// SigScheme represents a signing scheme.
+type SigScheme struct {
+	Alg   Algorithm `json:"algorithm"`
+	Hash  Algorithm `json:"hash"`
+	Count uint32    `json:"count"`
+}
+
+// KDFScheme represents a key derivation function scheme.
+type KDFScheme struct {
+	Alg  Algorithm `json:"algorithm"`
+	Hash Algorithm `json:"hash"`
+}
+
+// ECPoint represents ECC coordinates for a point.
+type ECPoint struct {
+	X *big.Int `json:"x,omitempty"`
+	Y *big.Int `json:"y,omitempty"`
+}
+
+// ToPublic converts to a corresponding tpm2 object.
+func (t PublicTemplate) ToPublic() tpm2.Public {
+	rv := tpm2.Public{
+		Type:    tpm2.Algorithm(t.Type),
+		NameAlg: tpm2.Algorithm(t.NameAlg),
+	}
+
+	for _, p := range t.Attributes {
+		rv.Attributes |= tpm2.KeyProp(p)
+	}
+
+	if t.RSAParameters != nil {
+		rv.RSAParameters = t.RSAParameters.ToPublic()
+	}
+
+	if t.ECCParameters != nil {
+		rv.ECCParameters = t.ECCParameters.ToPublic()
+	}
+
+	if t.SymCipherParameters != nil {
+		rv.SymCipherParameters = t.SymCipherParameters.ToPublic()
+	}
+
+	if t.KeyedHashParameters != nil {
+		rv.KeyedHashParameters = t.KeyedHashParameters.ToPublic()
+	}
+
+	return rv
+}
+
+// ToPublic converts to a corresponding tpm2 object.
+func (p RSAParams) ToPublic() *tpm2.RSAParams {
+	rv := &tpm2.RSAParams{
+		KeyBits:     p.KeyBits,
+		ExponentRaw: p.Exponent,
+	}
+
+	if p.Symmetric != nil {
+		rv.Symmetric = p.Symmetric.ToPublic()
+	}
+
+	if p.Sign != nil {
+		rv.Sign = p.Sign.ToPublic()
+	}
+
+	if p.Modulus != nil {
+		mBytes := p.Modulus.Bytes()
+
+		buf := make([]byte, len(mBytes)+2)
+		binary.BigEndian.PutUint16(buf[0:2], uint16(len(mBytes)))
+		copy(buf[2:], mBytes)
+
+		rv.ModulusRaw = tpmutil.U16Bytes(mBytes)
+	}
+
+	return rv
+}
+
+// ToPublic converts to a corresponding tpm2 object.
+func (p ECCParams) ToPublic() *tpm2.ECCParams {
+	rv := &tpm2.ECCParams{
+		CurveID: tpm2.EllipticCurve(p.CurveID),
+	}
+
+	if p.Symmetric != nil {
+		rv.Symmetric = p.Symmetric.ToPublic()
+	}
+
+	if p.Sign != nil {
+		rv.Sign = p.Sign.ToPublic()
+	}
+
+	if p.KDF != nil {
+		rv.KDF = p.KDF.ToPublic()
+	}
+
+	if p.Point != nil {
+		rv.Point = p.Point.ToPublic()
+	}
+
+	return rv
+}
+
+// ToPublic converts to a corresponding tpm2 object.
+func (p SymCipherParams) ToPublic() *tpm2.SymCipherParams {
+	rv := &tpm2.SymCipherParams{}
+
+	if p.Symmetric != nil {
+		rv.Symmetric = p.Symmetric.ToPublic()
+	}
+
+	return rv
+}
+
+// ToPublic converts to a corresponding tpm2 object.
+func (p KeyedHashParams) ToPublic() *tpm2.KeyedHashParams {
+	return &tpm2.KeyedHashParams{
+		Alg:  tpm2.Algorithm(p.Alg),
+		Hash: tpm2.Algorithm(p.Hash),
+		KDF:  tpm2.Algorithm(p.KDF),
+	}
+}
+
+// ToPublic converts to a corresponding tpm2 object.
+func (s SymScheme) ToPublic() *tpm2.SymScheme {
+	return &tpm2.SymScheme{
+		Alg:     tpm2.Algorithm(s.Alg),
+		KeyBits: s.KeyBits,
+		Mode:    tpm2.Algorithm(s.Mode),
+	}
+}
+
+// ToPublic converts to a corresponding tpm2 object.
+func (s SigScheme) ToPublic() *tpm2.SigScheme {
+	return &tpm2.SigScheme{
+		Alg:   tpm2.Algorithm(s.Alg),
+		Hash:  tpm2.Algorithm(s.Hash),
+		Count: s.Count,
+	}
+}
+
+// ToPublic converts to a corresponding tpm2 object.
+func (s KDFScheme) ToPublic() *tpm2.KDFScheme {
+	return &tpm2.KDFScheme{
+		Alg:  tpm2.Algorithm(s.Alg),
+		Hash: tpm2.Algorithm(s.Hash),
+	}
+}
+
+// ToPublic converts to a corresponding tpm2 object.
+func (s ECPoint) ToPublic() tpm2.ECPoint {
+	var rv tpm2.ECPoint
+
+	if s.X != nil {
+		nBytes := s.X.Bytes()
+
+		buf := make([]byte, len(nBytes)+2)
+		binary.BigEndian.PutUint16(buf[0:2], uint16(len(nBytes)))
+		copy(buf[2:], nBytes)
+
+		rv.XRaw = tpmutil.U16Bytes(nBytes)
+	}
+
+	if s.Y != nil {
+		nBytes := s.Y.Bytes()
+
+		buf := make([]byte, len(nBytes)+2)
+		binary.BigEndian.PutUint16(buf[0:2], uint16(len(nBytes)))
+		copy(buf[2:], nBytes)
+
+		rv.YRaw = tpmutil.U16Bytes(nBytes)
+	}
+
+	return rv
 }
--- public_template_test.go
+++ public_template_test.go
@@ -3,8 +3,11 @@ package pgtpm_test
 import (
 	"encoding/json"
 	"io/ioutil"
+	"math/big"
 	"reflect"
 	"testing"
+
+	"github.com/google/go-tpm/tpm2"
 
 	"github.com/paulgriffiths/pgtpm"
 )
@@ -29,6 +32,52 @@ func TestPublicTemplateUnmarshalJSON(t *testing.T) {
 					pgtpm.TPMA_OBJECT_FIXEDPARENT,
 					pgtpm.TPMA_OBJECT_SENSITIVEDATAORIGIN,
 				},
+				RSAParameters: &pgtpm.RSAParams{
+					Symmetric: &pgtpm.SymScheme{
+						Alg:     pgtpm.TPM2_ALG_AES,
+						KeyBits: 128,
+						Mode:    pgtpm.TPM2_ALG_CFB,
+					},
+					Sign: &pgtpm.SigScheme{
+						Alg:  pgtpm.TPM2_ALG_RSAPSS,
+						Hash: pgtpm.TPM2_ALG_SHA256,
+					},
+					KeyBits:  2048,
+					Exponent: 65537,
+					Modulus:  big.NewInt(102387),
+				},
+				ECCParameters: &pgtpm.ECCParams{
+					Symmetric: &pgtpm.SymScheme{
+						Alg:     pgtpm.TPM2_ALG_AES,
+						KeyBits: 256,
+						Mode:    pgtpm.TPM2_ALG_OFB,
+					},
+					Sign: &pgtpm.SigScheme{
+						Alg:  pgtpm.TPM2_ALG_ECDSA,
+						Hash: pgtpm.TPM2_ALG_SHA256,
+					},
+					CurveID: pgtpm.TPM2_ECC_NIST_P256,
+					KDF: &pgtpm.KDFScheme{
+						Alg:  pgtpm.TPM2_ALG_KDF1_SP800_108,
+						Hash: pgtpm.TPM2_ALG_SHA384,
+					},
+					Point: &pgtpm.ECPoint{
+						X: big.NewInt(42),
+						Y: big.NewInt(99),
+					},
+				},
+				SymCipherParameters: &pgtpm.SymCipherParams{
+					Symmetric: &pgtpm.SymScheme{
+						Alg:     pgtpm.TPM2_ALG_TDES,
+						KeyBits: 64,
+						Mode:    pgtpm.TPM2_ALG_CBC,
+					},
+				},
+				KeyedHashParameters: &pgtpm.KeyedHashParams{
+					Alg:  pgtpm.TPM2_ALG_HMAC,
+					Hash: pgtpm.TPM2_ALG_SHA512,
+					KDF:  pgtpm.TPM2_ALG_KDF1_SP800_56A,
+				},
 			},
 		},
 	}
@@ -51,6 +100,55 @@ func TestPublicTemplateUnmarshalJSON(t *testing.T) {
 
 			if !reflect.DeepEqual(got, tc.want) {
 				t.Errorf("got %v, want %v", got, tc.want)
+			}
+		})
+	}
+}
+
+func TestPublicTemplateToPublic(t *testing.T) {
+	t.Parallel()
+
+	var testcases = []struct {
+		tmpl string
+		pub  string
+	}{
+		{tmpl: "testdata/rsa_key.json", pub: "testdata/rsa_key.public"},
+		{tmpl: "testdata/ecc_key.json", pub: "testdata/ecc_key.public"},
+		{tmpl: "testdata/rsa_storage.json", pub: "testdata/rsa_storage.public"},
+	}
+
+	for _, tc := range testcases {
+		var tc = tc
+
+		t.Run(tc.tmpl, func(t *testing.T) {
+			t.Parallel()
+
+			data, err := ioutil.ReadFile(tc.tmpl)
+			if err != nil {
+				t.Fatalf("couldn't read template from file: %v", err)
+			}
+
+			var tmpl pgtpm.PublicTemplate
+			if err := json.Unmarshal(data, &tmpl); err != nil {
+				t.Fatalf("couldn't unmarshal template: %v", err)
+			}
+
+			got := tmpl.ToPublic()
+
+			data, err = ioutil.ReadFile(tc.pub)
+			if err != nil {
+				t.Fatalf("couldn't read public area from file: %v", err)
+			}
+
+			want, err := tpm2.DecodePublic(data)
+			if err != nil {
+				t.Fatalf("couldn't decode public area: %v", err)
+			}
+
+			if !reflect.DeepEqual(got, want) {
+				t.Errorf("got %v, want %v", got, want)
+				t.Errorf("got %v, want %v", got.RSAParameters, want.RSAParameters)
+				t.Errorf("got %v, want %v", got.RSAParameters.Symmetric, want.RSAParameters.Symmetric)
 			}
 		})
 	}
